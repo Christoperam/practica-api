4137199c878fd84916beb796282b11ad09d53385
Christoperam <cciramirez@outlook.com>
2022-08-17 15:44:24 -0500

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,9 +13,13 @@ class HomeController extends Controller
         //$usuarios = HTTP::get('https://jsonplaceholder.typicode.com/comments');
         //$usuarios = HTTP::get('https://jsonplaceholder.typicode.com/users');
         //$usuarios = HTTP::get('https://mindicador.cl/api');
-        $usuarios = HTTP::get('https://kitsu.io/api/edge/anime');
+        //$usuarios = HTTP::get('https://kitsu.io/api/edge/anime');
+        $usuarios = HTTP::get('https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=dd0e4779ccbf458a30ea49eb49dd1836&hash=b988e4b8ec4cbd70c9f89fed8a8de772');
+
         $info = $usuarios->json();
-        $showArray = $info['data'];
-        return view('api')->with('showArray',$showArray);
+        $showArray = $info['data']['results'];
+
+        $x = false;
+        return view('api')->with('showArray', $showArray);
     }
 }
--- resources/views/api.blade.php
+++ resources/views/api.blade.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container fluid">
         <div class="row"></div>
         <br><br><br>
         <div class="row">
-            @foreach ($showArray as $item) 
-                <div class="col-sm-4">
+            @foreach ($showArray as $item)
+                <div class="col-sm-6">
                     <div class="card">
                         <div class="card-body">
                             <div class="row mb-3">
@@ -21,17 +24,36 @@
                                 <label for="" class="col-sm-4 col-form-label">ID</label>
                                 <div class="col-sm-1"></div>
                                 <div class="col-sm-4">
-                                    <input type="number" value="{{ $item['type'] }}">
-                                    <input type="number" value="{{ $item['links']}}">
+                                    {{ $item['id'] }}
                                 </div>
                                 <div class="col-sm-2"></div>
+                                <div class="col-sm-1"></div>
+                                <label for="" class="col-sm-4 col-form-label">Name</label>
+                                <div class="col-sm-1"></div>
+                                <div class="col-sm-4">
+                                    {{ $item['name'] }}
+                                </div>
+                                <div class="col-sm-2"></div>
+                                <div class="col-sm-1"></div>
+                                <label for="" class="col-sm-4 col-form-label">Description</label>
+                                <div class="col-sm-1"></div>
+                                <div class="col-sm-4">
+                                    {{ $item['description'] }}
+                                </div>
+                                <div class="col-sm-2"></div>
+
+
                             </div>
                         </div>
                     </div>
+                    <br>
                 </div>
             @endforeach
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
